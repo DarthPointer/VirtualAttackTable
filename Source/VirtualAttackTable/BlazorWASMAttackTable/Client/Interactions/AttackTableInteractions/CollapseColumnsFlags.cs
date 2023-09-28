bf7a9daba07142ccfe5d5d1311338974438773a6
DarthPointer <al.kiriackin@yandex.ru>
2023-09-28 21:13:27 +0600

    Collapsing columns - Created icos for collapsed attack table columns. - Created a class to store collapse flag interactions.

---
+++ Source/VirtualAttackTable/BlazorWASMAttackTable/Client/Interactions/AttackTableInteractions/CollapseColumnsFlags.cs
@@ -0,0 +1,22 @@
+﻿using BlazorWASMAttackTable.Client.Interactions.Common;
+
+namespace BlazorWASMAttackTable.Client.Interactions.AttackTableInteractions
+{
+    public class CollapseColumnsFlags
+    {
+        public ValueInteraction<bool> CollapseBearing { get; } = new(false);
+        public ValueInteraction<bool> CollapseAbsoluteHeight { get; } = new(false);
+        public ValueInteraction<bool> CollapseVisibleHeight { get; } = new(false);
+        public ValueInteraction<bool> CollapseAbsoluteLength { get; } = new(false);
+        public ValueInteraction<bool> CollapseVisibleLength { get; } = new(false);
+        public ValueInteraction<bool> CollapseTargetRange { get; } = new(false);
+        public ValueInteraction<bool> CollapseAoB { get; } = new(false);
+        public ValueInteraction<bool> CollapseHullTime { get; } = new(false);
+        public ValueInteraction<bool> CollapseOneDegreeTime { get; } = new(false);
+        public ValueInteraction<bool> CollapseTargetSpeed { get; } = new(false);
+        public ValueInteraction<bool> CollapseAngularTargetSpeed { get; } = new(false);
+        public ValueInteraction<bool> CollapseTorpedoSpeed { get; } = new(false);
+        public ValueInteraction<bool> CollapseBoatSpeed { get; } = new(false);
+        public ValueInteraction<bool> CollapseLeadAngle { get; } = new(false);
+    }
+}
